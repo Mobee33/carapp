29887b84db8ca84ea499ae43707e176c354595e7
Mubeen Bankur <mobee33@hotmail.com>
2022-08-18 14:00:13 +0400

    added review model

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
   root to: "pages#home"
-  get "cars", to: "cars#index"
-  get "cars/new", to: "cars#new", as: :new_car
-  post "cars", to: "cars#create"
-  get "cars/:id", to: "cars#show", as: :car
-  get "cars/:id/edit", to: "cars#edit", as: :edit_car
-  patch "cars/:id", to: "cars#update"
-  delete "cars/:id", to: "cars#destroy"
+  resources :cars
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_18_082458) do
+ActiveRecord::Schema.define(version: 2022_08_18_095854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,4 +24,14 @@ ActiveRecord::Schema.define(version: 2022_08_18_082458) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "comment"
+    t.integer "rating"
+    t.bigint "car_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["car_id"], name: "index_reviews_on_car_id"
+  end
+
+  add_foreign_key "reviews", "cars"
 end
