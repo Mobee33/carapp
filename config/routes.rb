f82bfe78fd1667e812f7a22aa6a7351bb09d79c4
Mubeen Bankur <mobee33@hotmail.com>
2022-08-18 13:49:19 +0400

    added crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root to: "pages#home"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "cars", to: "cars#index"
+  get "cars/new", to: "cars#new", as: :new_car
+  post "cars", to: "cars#create"
+  get "cars/:id", to: "cars#show", as: :car
+  get "cars/:id/edit", to: "cars#edit", as: :edit_car
+  patch "cars/:id", to: "cars#update"
+  delete "cars/:id", to: "cars#destroy"
 end
