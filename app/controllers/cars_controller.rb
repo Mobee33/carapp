2a3e87c242611d42488242a725875c4c834c5a42
Mubeen Bankur <mobee33@hotmail.com>
2022-08-19 12:04:08 +0400

    added validations

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -41,7 +41,7 @@ class CarsController < ApplicationController
   private
 
   def car_params
-    params.require(:car).permit(:name, :model, :price, :is_available)
+    params.require(:car).permit(:name, :model, :price, :is_available, :image_url)
   end
 
   def set_car
--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,7 @@
 class Car < ApplicationRecord
   has_many :reviews
+
+  validates :name, presence: true
+  validates :model, presence: true
+  validates :price, presence: true
 end
